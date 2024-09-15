cec7410b7f066289c45d34537638603d28e997ff
ayodeji <ayodejiakintobi1@gmail.com>
2024-09-15 17:53:03 +0100

    UPDATE: CONTRACT ADDED

--- src/whitelist.sol
+++ src/whitelist.sol
@@ -1,7 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-contract Whitelist {
-    uint8 public  MaxAllowedWhiteList;
+contract whitelist {
+    uint8 public MaxAllowedWhiteList;
+    uint8 public numAddressesListed; //storing listed addresses
 
+    error NotWhiteListedAddres();
+    error WhiteListFilled();
+
+    mapping(address => bool) public WhiteListedAddresses; //maps to check if address has been added or not
+
+    constructor(uint8 _MaxAllowedWhiteList) {
+        MaxAllowedWhiteList = _MaxAllowedWhiteList;
+    }
+
+    function addWhitelistAddress() public {
+        if (!WhiteListedAddresses[msg.sender]) {
+            revert NotWhiteListedAddres();
+        }
+
+        if (numAddressesListed >= MaxAllowedWhiteList) {
+            revert WhiteListFilled();
+        }
+        WhiteListedAddresses[msg.sender] = true; //confirming the sneder from bool
+        numAddressesListed++;
+    }
 }
